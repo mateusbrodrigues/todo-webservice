c22eb510a07a13147bbefb0a6b5aaaef1ffaeea5
Mateus Rodrigues <mateusborges090@gmail.com>
2023-10-14 17:14:23 -0300

    Todo-webservice

--- src/main/java/dev/mateusb/todolist/task/TaskController.java
+++ src/main/java/dev/mateusb/todolist/task/TaskController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
