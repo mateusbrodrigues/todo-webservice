289afc389d0143c35bf2bdd66dc2f7bb58df354d
Mateus Rodrigues <mateusborges090@gmail.com>
2023-10-14 17:13:42 -0300

    Todo-webservice

---
+++ src/test/java/dev/mateusb/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mateusb.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
